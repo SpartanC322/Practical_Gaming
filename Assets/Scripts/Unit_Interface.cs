9a21bc3f4f7195e17f417526aef000cf5937383e
Ben Lenihan <benlenihan99@gmail.com>
2020-05-06 05:14:08 +0100

    Abstract and Interface Update

---
+++ Assets/Scripts/Unit_Interface.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Unit_Interface 
+{
+    void Set_Heavy();
+    void Set_Normal();
+    void Set_Ranged();
+    void set_Player_2();
+    bool get_Player_2();
+    void set_Health(int new_Health);
+    int get_Health();
+    void has_Moved();
+    int get_Attack_Range();
+    void has_Not_Moved();
+    bool has_It_Moved();
+    void set_Class_Selected();
+    int get_Damage();
+    string get_Class();
+    int get_Distance_Can_Move();
+    bool is_Class_Selected();
+}
